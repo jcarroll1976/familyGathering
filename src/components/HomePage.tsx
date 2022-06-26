bcd7fe6d6083690a48a56e23526a710f58debafa
Joshua Carroll <carrolljosh1976@gmail.com>
2022-06-26 19:11:38 -0400

    Added more details

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,7 +5,13 @@ function Homepage() {
     const [guests,setGuests] = useState<Guests>()
 
     return (
+        <div className='Homepage'>
         <h1>Ulis Family Gathering</h1>
+        <p>Come gather with family for lunch and to catch up!</p>
+        <p>Hay Rides!</p>
+        <p>Pick your own pumpkin for Halloween!</p>
+
+        </div>
     )
 }
 
